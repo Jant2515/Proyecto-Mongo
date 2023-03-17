f7ad2efe44d1a21ff9654eb51a1fb52273d44482
VictorJCruz <98476822+VictorJCruz@users.noreply.github.com>
2023-03-17 14:50:46 -0600

    Commit.v2

---
+++ View/blog.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once __DIR__ . '\generales.php';
+?>
+  <head>
+    <?php
+   links2();
+    ?>
+</head>
+
+<body>
+    <form role="form" action="" method="post">
+        <?php
+        navBar2();
+        ?>
+        </header>
+
+  
+
+   
+
+    <div class="site-blocks-cover inner-page-cover" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+              <h1 class="text-white font-weight-light">Blog Posts</h1>
+              <div><a href="../index.html">Home</a> <span class="mx-2 text-white">&bullet;</span> <span class="text-white">Blog</span></div>
+              
+            </div>
+          </div>
+        </div>
+      </div>  
+
+
+    
+    <div class="site-section">
+      <div class="container">
+        
+        <div class="row mb-3 align-items-stretch">
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div> 
+          </div>
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_2.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_2.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div> 
+          </div>
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div>
+          </div>
+          
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div> 
+          </div>
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_2.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">How to Plan Your Vacation</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <a href="#" class="btn btn-outline-primary border-2 py-3 px-5">Load More Posts...</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <footer>
+            <?php
+            Footer2();
+            ?>
+        </footer>
+        </div>
+        <?php
+        allLinks2();
+        ?>
+    
+  </body>
+    
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,111 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+include_once __DIR__ . '\View\generales.php';
+?>
   <head>
-    <title>Travalers &mdash; Colorlib Website Template</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
-    <link rel="stylesheet" href="View/fonts/icomoon/style.css">
+    <?php
+   links();
+    ?>
+</head>
 
-    <link rel="stylesheet" href="View/css/bootstrap.min.css">
-    <link rel="stylesheet" href="View/css/magnific-popup.css">
-    <link rel="stylesheet" href="View/css/jquery-ui.css">
-    <link rel="stylesheet" href="View/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="View/css/owl.theme.default.min.css">
+<body>
+    <form role="form" action="" method="post">
+        <?php
+        navBar();
+        ?>
+        </header>
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
-
-
-    <link rel="stylesheet" href="View/css/aos.css">
-
-    <link rel="stylesheet" href="View/css/style.css">
-    
-  </head>
-  <body>
-  
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-    
-    <header class="site-navbar py-1" role="banner">
-
-      <div class="container">
-        <div class="row align-items-center">
-          
-          <div class="col-6 col-xl-2">
-            <h1 class="mb-0"><a href="index.html" class="text-black h2 mb-0">Travelers</a></h1>
-          </div>
-          <div class="col-10 col-md-8 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right text-lg-center" role="navigation">
-
-              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
-                <li class="active">
-                  <a href="index.html">Home</a>
-                </li>
-                <li class="has-children">
-                  <a href="View/destination.html">Destinations</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Japan</a></li>
-                    <li><a href="#">Europe</a></li>
-                    <li><a href="#">China</a></li>
-                    <li><a href="#">France</a></li>
-                  </ul>
-                </li>
-                
-                <li><a href="View/discount.html">Discount</a></li>
-                <li><a href="View/about.html">About</a></li>
-                <li><a href="View/blog.html">Blog</a></li>
-                
-                <li><a href="View/contact.html">Contact</a></li>
-                <!-- <li><a href="booking.html">Book Online</a></li> -->
-              </ul>
-            </nav>
-          </div>
-
-          <div class="col-6 col-xl-2 text-right">
-            <div class="d-none d-xl-inline-block">
-              <ul class="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0" data-class="social">
-                <li>
-                  <a href="#" class="pl-0 pr-3 text-black"><span class="icon-tripadvisor"></span></a>
-                </li>
-                <li>
-                  <a href="#" class="pl-3 pr-3 text-black"><span class="icon-twitter"></span></a>
-                </li>
-                <li>
-                  <a href="#" class="pl-3 pr-3 text-black"><span class="icon-facebook"></span></a>
-                </li>
-                <li>
-                  <a href="#" class="pl-3 pr-3 text-black"><span class="icon-instagram"></span></a>
-                </li>
-                
-              </ul>
-            </div>
-
-            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-          </div>
-
-        </div>
-      </div>
-      
-    </header>
-
-  
-
+    <!--Fin de navbar-->
    
-
+<!--Carousel-->
     <div class="slide-one-item home-slider owl-carousel">
       
       <div class="site-blocks-cover overlay" style="background-image: url(View/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
@@ -139,6 +53,7 @@
 
     </div>
 
+      <!--Fin carousel-->
 
     <div class="site-section">
       
@@ -487,100 +402,14 @@
       </div>
     </div>
     
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="mb-5">
-              <h3 class="footer-heading mb-4">About Travelers</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque, consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima minus odio!</p>
-            </div>
-
-            
-            
-          </div>
-          <div class="col-lg-4 mb-5 mb-lg-0">
-            <div class="row mb-5">
-              <div class="col-md-12">
-                <h3 class="footer-heading mb-4">Navigations</h3>
-              </div>
-              <div class="col-md-6 col-lg-6">
-                <ul class="list-unstyled">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">Destination</a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">About</a></li>
-                </ul>
-              </div>
-              <div class="col-md-6 col-lg-6">
-                <ul class="list-unstyled">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                  <li><a href="#">Discounts</a></li>
-                </ul>
-              </div>
-            </div>
-
-            
-
-          </div>
-
-          <div class="col-lg-4 mb-5 mb-lg-0">
-           
-
-            <div class="mb-5">
-              <h3 class="footer-heading mb-2">Subscribe Newsletter</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit minima minus odio.</p>
-
-              <form action="#" method="post">
-                <div class="input-group mb-3">
-                  <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
-                  <div class="input-group-append">
-                    <button class="btn btn-primary text-white" type="button" id="button-addon2">Send</button>
-                  </div>
-                </div>
-              </form>
-
-            </div>
-
-          </div>
-          
-        </div>
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <div class="mb-5">
-              <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-            </div>
-
-            <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-          </div>
-          
-        </div>
-      </div>
-    </footer>
-  </div>
-
-  <script src="View/js/jquery-3.3.1.min.js"></script>
-  <script src="View/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="View/js/jquery-ui.js"></script>
-  <script src="View/js/popper.min.js"></script>
-  <script src="View/js/bootstrap.min.js"></script>
-  <script src="View/js/owl.carousel.min.js"></script>
-  <script src="View/js/jquery.stellar.min.js"></script>
-  <script src="View/js/jquery.countdown.min.js"></script>
-  <script src="View/js/jquery.magnific-popup.min.js"></script>
-  <script src="View/js/bootstrap-datepicker.min.js"></script>
-  <script src="View/js/aos.js"></script>
-
-  <script src="View/js/main.js"></script>
+    <footer>
+            <?php
+            Footer();
+            ?>
+        </footer>
+        <?php
+        allLinks();
+        ?>
     
   </body>
 </html>
